550c5d47f230950ccfdf3c396d3b15ca25ab912a
Brennum1990 <111984560+Brennum1990@users.noreply.github.com>
2022-12-05 16:40:16 +0100

    put rating js on hold

--- js/modules/rating.js
+++ js/modules/rating.js
@@ -1,16 +1,16 @@
-export default function() {
-	const allStars = document.querySelectorAll('.star');
+// export default function() {
+// 	const allStars = document.querySelectorAll('.star');
 
-	allStars.forEach((star, i) => {
-		star.onclick = function () {
-			let currentStarLevel = i + 1;
+// 	allStars.forEach((star, i) => {
+// 		star.onclick = function () {
+// 			let currentStarLevel = i + 1;
 
-			allStars.forEach((star, j) => {
-				if(currentStarLevel >= j + 1) {
-					star.innerHTML = 
+// 			allStars.forEach((star, j) => {
+// 				if(currentStarLevel >= j + 1) {
+// 					star.innerHTML = 
 
-				} 
-			})
-		}
-	})
-}
+// 				} 
+// 			})
+// 		}
+// 	})
+// }
