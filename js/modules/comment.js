4f474b078dc9b7be4484a1f1cdf7e916c96c9766
Brennum1990 <111984560+Brennum1990@users.noreply.github.com>
2022-12-08 17:57:08 +0100

    added and removed code for comment section

--- js/modules/comment.js
+++ js/modules/comment.js
@@ -1,81 +1,43 @@
-export default function(Comment) {
+export default function(comment) {
+	const field = document.querySelector('textarea');
+	const backUp = field.getAttribute('placeholder')
+	const btn = document.querySelector('.btn');
+	const clear = document.getElementById('clear')
+	const submit = document.querySelector('#submit')
+	// const comments = document.querySelector('#comment-box')
+	const comments = document.getElementById('comment-box');
 	
-	window.onload = function setTemplate() {
-		document.getElementById('allComments').innerHTML = localStorage.getItem('template');
-  };
-  
-  const commentContainer = document.getElementById('allComments');
-  document.getElementById('addComments').addEventListener('click', function (ev) {
-	  addComment(ev);
-  });
-  
-  function addComment(ev) {
-		let commentText, wrapDiv;
-		const textBox = document.createElement('div');
-		const replyButton = document.createElement('button');
-		replyButton.className = 'reply';
-		replyButton.innerHTML = 'Reply';
-		const likeButton = document.createElement('button');
-		likeButton.innerHTML = 'Like';
-		likeButton.className = 'likeComment';
-		const deleteButton = document.createElement('button');
-		deleteButton.innerHTML = 'Delete';
-		deleteButton.className = 'deleteComment';
-		if(hasClass(ev.target.parentElement, 'container')) {
-			 const wrapDiv = document.createElement('div');
-			 wrapDiv.className = 'wrapper';
-			 wrapDiv.style.marginLeft = 0;
-			 commentText = document.getElementById('comment').value;
-			 document.getElementById('comment').value = '';
-			 textBox.innerHTML = commentText;
-			 textBox.style.backgroundColor = "cornflowerblue";
-			 wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
-			 commentContainer.appendChild(wrapDiv);
-		} else {
-			 wrapDiv = ev.target.parentElement;
-			 commentText = ev.target.parentElement.firstElementChild.value;
-			 textBox.innerHTML = commentText;
-			 textBox.style.backgroundColor = "paleturquoise";
-			 wrapDiv.innerHTML = '';
-			 wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
-		}
-		setOnLocalStorage();
-  }
-  
-  function setOnLocalStorage () {
-		localStorage.setItem('template', document.getElementById('allComments').innerHTML);
-  }
-  function hasClass(elem, className) {
-		return elem.className.split(' ').indexOf(className) > -1;
-  }
-  document.getElementById('allComments').addEventListener('click', function (e) {
-		if (hasClass(e.target, 'reply')) {
-			 const parentDiv = e.target.parentElement;
-			 const wrapDiv = document.createElement('div');
-			 wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
-			 wrapDiv.className = 'wrapper';
-			 const textArea = document.createElement('textarea');
-			 textArea.style.marginRight = '20px';
-			 const addButton = document.createElement('button');
-			 addButton.className = 'addReply';
-			 addButton.innerHTML = 'Add';
-			 const cancelButton = document.createElement('button');
-			 cancelButton.innerHTML = 'Cancel';
-			 cancelButton.className='cancelReply';
-			 wrapDiv.append(textArea, addButton, cancelButton);
-			 parentDiv.appendChild(wrapDiv);
-  
-		} else if(hasClass(e.target, 'addReply')) {
-			 addComment(e);
-		} else if(hasClass(e.target, 'likeComment')) {
-			  const likeBtnValue = e.target.innerHTML;
-			  e.target.innerHTML = likeBtnValue !== 'Like' ? Number.parseInt(likeBtnValue) + 1 : 1;
-			 setOnLocalStorage();
-		} else if(hasClass(e.target, 'cancelReply')) {
-			 e.target.parentElement.innerHTML = '';
-			 setOnLocalStorage();
-		} else if(hasClass(e.target, 'deleteComment')) {
-			 e.target.parentElement.remove();
-		}
-  });
+	// array to store the comments
+	const comments_arr = [];
+	
+	// to generate html list based on comments array
+	const display_comments = () => {
+	  let list = '<ul>';
+		comments_arr.forEach(comment => {
+		 list += `<li>${comment}</li>`;
+	  })
+	  list += '</ul>';
+	  comments.innerHTML = list;
+	}
+	
+	clear.onclick = function(event){
+	  event.preventDefault();
+	  // reset the array  
+		comments_arr.length = 0;
+	  // re-genrate the comment html list
+	  display_comments();
+	}
+	
+	submit.onclick = function(event){
+		 event.preventDefault();
+		 const content = field.value;
+		 if(content.length > 0){ // if there is content
+			// add the comment to the array
+			comments_arr.push(content);
+			// re-genrate the comment html list
+			display_comments();
+			// reset the textArea content 
+			field.value = '';
+		 }
+	}
 }
